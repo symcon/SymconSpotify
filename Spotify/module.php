6b0478258db5e435e18052d4ebb28e7a37fbeb12
Niels Thole <nt@symcon.de>
2020-07-21 10:47:14 +0200

    Fix style

--- Spotify/module.php
+++ Spotify/module.php
@@ -477,14 +477,14 @@ declare(strict_types=1);
                                 if (isset($currentPlay['item']['album']['images'])) {
                                     foreach ($currentPlay['item']['album']['images'] as &$imageObject) {
                                         if ((($imageObject['height'] <= $this->ReadPropertyInteger('CoverMaxHeight')) || ($this->ReadPropertyInteger('CoverMaxHeight') == 0)) &&
-                                        (($imageObject['width'] <= $this->ReadPropertyInteger('CoverMaxWidth')) ||  ($this->ReadPropertyInteger('CoverMaxWidth') == 0))) {
+                                        (($imageObject['width'] <= $this->ReadPropertyInteger('CoverMaxWidth')) || ($this->ReadPropertyInteger('CoverMaxWidth') == 0))) {
                                             $coverFound = true;
                                             $newValue = '<iframe style="border: 0;" height="' . $imageObject['height'] . '" width = "' . $imageObject['width'] . '" src="' . $imageObject['url'] . '">';
                                             if ($this->GetValue('CurrentCover') != $newValue) {
                                                 $this->SetValue('CurrentCover', $newValue);
                                             }
                                             break;
-                                        }   
+                                        }
                                     }
                                 }
                                 if (!$coverFound && ($this->GetValue('CurrentCover')) != '') {
